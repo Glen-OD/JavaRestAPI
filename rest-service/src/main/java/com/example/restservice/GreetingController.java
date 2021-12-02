d191c9132770494d6eebe81eae55e08ddfde11ee
40233306 <godonovan01@qub.ac.uk>
2021-12-02 13:13:52 +0000

    ci change + response headers

--- rest-service/src/main/java/com/example/restservice/GreetingController.java
+++ rest-service/src/main/java/com/example/restservice/GreetingController.java
@@ -1,6 +1,8 @@
 package com.example.restservice;
 
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,13 +19,16 @@ public class GreetingController {
     // /greeting?name=Glen will print "hello Glen"
     @CrossOrigin(origins = "http://localhost:8080")
     @GetMapping("/")
-    public Greeting greeting(@RequestParam(required = false, value = "sentence", defaultValue = "0") String sentence){
+    public Greeting greeting(@RequestParam(required = false, value = "sentence", defaultValue = "0") String sentence, HttpServletResponse response){
         //Greeting ouput = new Greeting(counter.incrementAndGet(), String.format(template,sentence));
         //counter.decrementAndGet();
        // double avg = ouput.getAverageLength(sentence);
         //System.out.println(avg);
         //System.out.println(sentence);
         //example localhost:8080/?sentence=test hello = 4.5
+
+        response.addHeader("Access-Control-Allow-Origin", "*");
+        response.addHeader("Content-type","application/json");
 
 
         Greeting dummy = new Greeting(sentence, 5);
